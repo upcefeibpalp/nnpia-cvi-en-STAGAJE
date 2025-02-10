b1af405e5ad41ace841b03d3af3163fdc4de2057
STAGAJE <st64113@student.upce.cz>
2025-02-10 15:29:10 +0100

    CV-01: New Spring Boot module for backend.

---
+++ backend/src/main/java/cz/st64113/nnpia25/Nnpia25Application.java
@@ -0,0 +1,13 @@
+package cz.st64113.nnpia25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nnpia25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nnpia25Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cz/st64113/nnpia25/Nnpia25ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.st64113.nnpia25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nnpia25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
