e41fcfd5d3da2ee513fd84e1558e120e486d9460
STAGAJE <st64113@student.upce.cz>
2025-03-10 15:00:00 +0100

    CV-05: Worked out first half of excercises according to the assignment in /docs/CV05.md

--- backend/src/main/java/cz/st64113/nnpia25/controller/UserController.java
+++ backend/src/main/java/cz/st64113/nnpia25/controller/UserController.java
@@ -1,15 +1,13 @@
 package cz.st64113.nnpia25.controller;
 
 import cz.st64113.nnpia25.domain.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import cz.st64113.nnpia25.service.UserService;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
 
---
+++ backend/src/test/java/cz/st64113/nnpia25/controller/UserControllerTest.java
@@ -0,0 +1,69 @@
+package cz.st64113.nnpia25.controller;
+
+import cz.st64113.nnpia25.Nnpia25Application;
+import cz.st64113.nnpia25.domain.User;
+import cz.st64113.nnpia25.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Nnpia25Application.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        User user = new User(69L, "nnpia25", "nnpia25@gmail.com");
+        userRepository.save(user);
+        userRepository.flush();
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void findAllUsers() throws Exception {
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    void findUser() throws Exception {
+        mockMvc.perform(get("/api/v1/users/69"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(69)))
+                .andExpect(jsonPath("$.password", is("nnpia25")))
+                .andExpect(jsonPath("$.email", is("nnpia25@gmail.com")));
+    }
+
+    @Test
+    void findUser_whenGivenWrongId_Returns404() throws Exception {
+        mockMvc.perform(get("/api/v1/users/68"))
+                .andExpect(status().isNotFound());
+    }
+}
